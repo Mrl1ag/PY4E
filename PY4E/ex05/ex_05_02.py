e05bad23cf6499ee85f2b460a27ec506a9e4cf52
Marlon Brandão <62584653+marlonpf14@users.noreply.github.com>
2020-10-29 00:55:49 -0300

    Add files via upload

---
+++ PY4E/ex05/ex_05_02.py
@@ -0,0 +1,31 @@
+# 5.2 Write a program that repeatedly prompts a user for integer numbers until
+# the user enters 'done'. Once 'done' is entered, print out the largest and
+# smallest of the numbers. If the user enters anything other than a valid number
+# catch it with a try/except and put out an appropriate message and ignore the
+# number. Enter 7, 2, bob, 10, and 4 and match the output below.
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done" : break
+    else:
+        try:
+            num = int(num)
+        except:
+            print("Invalid input")
+            continue
+        for value in [num] :
+            if smallest is None :
+                smallest = value
+            elif value < smallest :
+                smallest = value
+        for value in [num] :
+            if largest is None :
+                largest = value
+            elif value > largest :
+                largest = value
+
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
